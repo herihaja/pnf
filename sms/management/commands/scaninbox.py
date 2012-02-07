501cb0f0790d0029db53dfd7933537f93386cbd2
roov7 <rova;ratovo@gmail.com>
2012-02-07 12:10:58 +0300

    updating cron

--- sms/management/commands/scaninbox.py
+++ sms/management/commands/scaninbox.py
@@ -8,15 +8,22 @@ import threading
 
 class Command(BaseCommand):
 
-    #def handle(self, *args, **options):
-    obj = Inbox.objects.filter(processed=False)
-    #n = 0
-    for row in obj:
-        # parser le message
-        try:
-            #process_sms(row.sendernumber, row.textdecoded, row.receivingdatetime, row.recipientid)
-            cron_process_sms(row)
-            #threading.Thread(target=cron_process_sms, args=(row,)).start()
-        except:
-            pass
-            #stderr.write('Error parsing message "%s"\n' % row.id)
+    def handle(self, *args, **options):
+        obj = Inbox.objects.filter(processed=False)
+        #n = 0
+        for row in obj:
+            # parser le message
+            try:
+                #process_sms(row.sendernumber, row.textdecoded, row.receivingdatetime, row.recipientid)
+                cron_process_sms(row)
+                #threading.Thread(target=cron_process_sms, args=(row,)).start()
+            except:
+                pass
+                #stderr.write('Error parsing message "%s"\n' % row.id)
+
+            # marquer comme traite
+            #row.processed = True
+            #row.save()
+            #n += 1
+
+        # stdout.write('Traitement de %s messages a %s \n' % (n, datetime.now()))
